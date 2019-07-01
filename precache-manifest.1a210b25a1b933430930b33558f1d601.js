8edbf8d2432cc40a9e39409bf0ac38cb3d89860e
Adriano Interaminense <adriano.interaminense@liferay.com>
2019-06-30 23:33:25 -0300

    Updates

---
+++ precache-manifest.1a210b25a1b933430930b33558f1d601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711c894a879f69b168b66c11024ed007",
+    "url": "/react-ui-thermometer/index.html"
+  },
+  {
+    "revision": "2b6515a41d7e9257c83b",
+    "url": "/react-ui-thermometer/static/css/main.c625b13b.chunk.css"
+  },
+  {
+    "revision": "13166b79c158ba62b181",
+    "url": "/react-ui-thermometer/static/js/2.7b23240b.chunk.js"
+  },
+  {
+    "revision": "2b6515a41d7e9257c83b",
+    "url": "/react-ui-thermometer/static/js/main.4843480b.chunk.js"
+  },
+  {
+    "revision": "bfe92d2787085f98ec17",
+    "url": "/react-ui-thermometer/static/js/runtime~main.9f4df80b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e62d569b635c2088da1cfb934aebc6f7.js"
+  "/react-ui-thermometer/precache-manifest.1a210b25a1b933430930b33558f1d601.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ui-thermometer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
